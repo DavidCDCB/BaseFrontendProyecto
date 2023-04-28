ace06234ea61beeabad3ccd9a6d2837dce2eb02f
Robin <robinson.1701625954@ucaldas.edu.co>
2023-04-28 11:27:41 -0500

    add interface payroll, mechanic and inconvenient

---
+++ src/app/core/models/Mechanic.interface.ts
@@ -0,0 +1,12 @@
+export interface IMechanic{
+  id: number;
+  name: string;
+  surname: string;
+  phone: string;
+  role: string;
+  email:string;
+  address: string;
+  commission: string;
+  salary: string;
+  userId: number;
+}
