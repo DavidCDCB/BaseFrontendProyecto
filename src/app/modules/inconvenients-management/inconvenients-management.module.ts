6f357bda3dde988ba1084b6df8943118ca9730f9
Robin <robinson.1701625954@ucaldas.edu.co>
2023-04-29 08:07:20 -0500

    add module inconvenients

---
+++ src/app/modules/inconvenients-management/inconvenients-management.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InconvenientsManagementComponent } from './inconvenients-management.component';
+
+
+
+@NgModule({
+  declarations: [
+    InconvenientsManagementComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class InconvenientsManagementModule { }
