078b6f042b02469ce34d911b42f0d82fbfd03836
Robin <robinson.1701625954@ucaldas.edu.co>
2023-05-05 15:00:47 -0500

    add payroll.component

--- src/app/modules/payrolls-management/payrolls-management.component.ts
+++ src/app/modules/payrolls-management/payrolls-management.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { IPayroll } from 'src/app/core/models/Payroll.interface';
+import { RequestsControllerService } from 'src/app/services/RequestsController.service';
+import Swal,{ SweetAlertIcon } from 'sweetalert2';
 
 @Component({
   selector: 'app-payrolls-management',
@@ -6,5 +9,95 @@ import { Component } from '@angular/core';
   styleUrls: ['./payrolls-management.component.scss']
 })
 export class PayrollsManagementComponent {
+
+  @Input() idPayroll!: number;
+  @ViewChild('childForm') childFormComponent!: PayrollFormComponent;
+  payrolls: IPayroll[] = [];
+  nameEntity: string = 'Payroll';
+
+  constructor(private HTTPPayroll: RequestsControllerService<IPayroll>) { }
+
+  ngOnInit(){
+    this.getPayrolls();
+  }
+
+  setIdPayroll(idPayroll: number): void {
+    this.idPayroll = idPayroll;
+  }
+
+  getPayrolls(): IPayroll[] {
+    this.HTTPPayroll.getElement(this.nameEntity).subscribe({
+      next: (payrolls: IPayroll[]) => {
+        this.payrolls = payrolls;
+      },
+      error: (error) => this.showToast('Error al obtener payrolles', 'error')
+    });
+    return this.payrolls!;
+  }
+
+  savePayroll(payroll: IPayroll): void {
+    if (!this.childFormComponent.isUpdate) {
+      this.HTTPPayroll.saveElement(this.nameEntity, payroll).subscribe(
+        (payroll: IPayroll) => {
+          console.log(payroll);
+          this.showToast('¡Amacenado correctamente!', 'success');
+          this.payrolls?.push(payroll);
+        }
+      )
+    } else {
+      this.HTTPPayroll.updateElement(this.nameEntity, payroll, this.childFormComponent.idForUpdate).subscribe(
+        (payroll: IPayroll) => {
+          console.log(payroll);
+          this.showToast('¡Actualizado correctamente!', 'info');
+          this.payrolls = this.payrolls?.map(x => x.id === payroll.id ? payroll : x);
+        }
+      )
+    }
+  }
+
+  deletePayroll(payroll: IPayroll): void {
+    this.showAlert('¿Realmente desea eliminar el registro?', () => {
+      this.HTTPPayroll.deleteElement(this.nameEntity, payroll.id).subscribe(
+        (payroll: IPayroll) => {
+          console.log(payroll);
+          this.showToast('¡Eliminado correctamente!', 'warning');
+          this.payrolls = this.payrolls?.filter(x => x.id !== payroll.id);
+        }
+      );
+    });
+  }
+
+  updatePayroll(payroll: IPayroll): void {
+    this.childFormComponent.idForUpdate = payroll.id;
+    this.childFormComponent.changeFields(payroll);
+  }
+
+  showToast(text: string, icon: SweetAlertIcon): void {
+    Swal.mixin({
+      toast: true,
+      position: 'top',
+      showConfirmButton: false,
+      timer: 3000,
+    }).fire({
+      icon: icon,
+      title: text
+    });
+  }
+
+  showAlert(text: string, action: Function): void {
+    Swal.fire({
+      title: text,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar',
+      confirmButtonText: 'SI'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        action();
+      }
+    });
+  }
 
 }
--- src/app/modules/payrolls-management/payrolls-management.module.ts
+++ src/app/modules/payrolls-management/payrolls-management.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PayrollsManagementComponent } from './payrolls-management.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -9,7 +10,9 @@ import { PayrollsManagementComponent } from './payrolls-management.component';
     PayrollsManagementComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class PayrollsManagementModule { }
