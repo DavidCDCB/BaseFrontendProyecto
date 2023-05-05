f06e47ed1c0a12f6b2e10f7f25c90bca86695eec
Robin <robinson.1701625954@ucaldas.edu.co>
2023-05-05 17:06:39 -0500

    end module payroll.component

--- src/app/modules/payrolls-management/components/payroll-table/payroll-table.component.ts
+++ src/app/modules/payrolls-management/components/payroll-table/payroll-table.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { IPayroll } from 'src/app/core/models/Payroll.interface';
 
 @Component({
   selector: 'app-payroll-table',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./payroll-table.component.scss']
 })
 export class PayrollTableComponent {
+  @Input()
+  listOfPayrolls?: IPayroll[] = [];
 
+  @Output()
+  onIdPayroll = new EventEmitter<number>();
+
+  @Output()
+  onDelete = new EventEmitter<IPayroll>();
+
+  @Output()
+  onUpdate = new EventEmitter<IPayroll>();
+
+  filter?: string;
+
+  constructor(private router: Router) { }
+
+  redirectToService(idPayroll: number): void {
+    this.router.navigate([`/services/${idPayroll}`]);
+  }
+
+  setIdPayroll(payroll: number): void {
+    this.onIdPayroll.emit(payroll);
+  }
+
+  sendIdDelete(payroll: IPayroll): void {
+    this.onDelete.emit(payroll);
+  }
+
+  sendIdUpdate(payroll: IPayroll): void {
+    this.onUpdate.emit(payroll);
+  }
+
+  filterList(): IPayroll[] | undefined {
+    return (this.filter) ? this.listOfPayrolls?.filter(payroll => {
+      return Object.values(payroll).join('').toLowerCase().includes(this.filter!.toLowerCase());
+    }).reverse() : this.listOfPayrolls!.reverse();
+  }
 }
